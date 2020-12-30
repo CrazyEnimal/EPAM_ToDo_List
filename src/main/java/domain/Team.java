28f6613969791da075e5377cf74e0e5f7fecd2b5
Egor Matveev <egormatveev2019@gmail.com>
2020-12-30 23:01:53 +0300

    Created model prototype

---
+++ src/main/java/domain/Team.java
@@ -0,0 +1,50 @@
+package domain;
+
+import java.io.Serializable;
+import java.util.Set;
+
+
+public class Team implements Serializable {
+    private int id;
+
+    private String title;
+    private Set<Member> members;
+    private Set<Project> projects;
+
+    public Team() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public Set<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<Member> members) {
+        this.members = members;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+}
