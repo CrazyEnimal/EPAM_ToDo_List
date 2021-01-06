c24fe971dbc6610229d31c1c39a9ece9c3d8dc0c
Egor Matveev <egormatveev2019@gmail.com>
2021-01-06 22:30:56 +0300

    Added annotation and links between entities Overriding equals, hasCode and toString Added loggers

--- src/main/java/domain/Member.java
+++ src/main/java/domain/Member.java
@@ -1,19 +1,55 @@
 package domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Table(name = "member")
 public class Member implements Serializable {
+    private static final Logger LOG = LoggerFactory.getLogger(Member.class);
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "MEMBER_ID", unique = true, nullable = false)
     private int id;
+
+    @Column(name = "USER_NAME", unique = true, nullable = false, length = 20)
     private String username;
+
+    @Column(name = "FIRST_NAME", unique = true, nullable = false, length = 20)
     private String firstName;
+
+    @Column(name = "LAST_NAME", unique = true, nullable = false, length = 20)
     private String lastName;
+
+    @Column(name = "EMAIL", unique = true, nullable = false, length = 70)
     private String email;
+
+    @Column(name = "PASSWORD", unique = true, nullable = false, length = 20)
     private String password;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "TEAM_ID", nullable = false)
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
     public Member() {
     }
-
 
     public int getId() {
         return id;
@@ -63,11 +99,37 @@ public class Member implements Serializable {
         this.password = password;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return id == member.id && username.equals(member.username) && firstName.equals(member.firstName) && lastName.equals(member.lastName) && email.equals(member.email) && password.equals(member.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
     public enum Role {
         SUPERUSER, USER, GUEST;
 
     }
 
+    @Enumerated(EnumType.STRING)
     private Role role;
 
 
--- src/main/java/domain/Project.java
+++ src/main/java/domain/Project.java
@@ -1,18 +1,59 @@
 package domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 
 public class Project implements Serializable {
+    private static final Logger LOG = LoggerFactory.getLogger(Project.class);
 
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "PROJECT_ID", unique = true, nullable = false)
     private int id;
+
+    @Column(name = "MEMBER", unique = true, nullable = false, length = 20)
     private Member creator;
+
+    @Column(name = "TITLE", unique = true, nullable = false, length = 20)
     private String title;
+
+    @Column(name = "SUPERVISOR", unique = true, nullable = false, length = 20)
     private Member supervisor;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "team")
     private Set<Member> members = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "team")
     private Set<Task> tasks = new HashSet<>();
+
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setProject(this);
+
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "TEAM_ID", nullable = false)
+    private Team team;
 
     public Project() {
     }
@@ -63,5 +104,30 @@ public class Project implements Serializable {
 
     public void setTasks(Set<Task> tasks) {
         this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", creator=" + creator +
+                ", title='" + title + '\'' +
+                ", supervisor=" + supervisor +
+                ", members=" + members +
+                ", tasks=" + tasks +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return id == project.id && creator.equals(project.creator) && title.equals(project.title) && supervisor.equals(project.supervisor) && members.equals(project.members) && tasks.equals(project.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, creator, title, supervisor, members, tasks);
     }
 }
