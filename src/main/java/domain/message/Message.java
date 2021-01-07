f40dd429358fa3b97435d11ea0391e3127cb180b
isvink <isvink@gmail.com>
2021-01-07 17:57:53 +0300

    Message added equals(); hashcode(); toString()

--- src/main/java/domain/message/Message.java
+++ src/main/java/domain/message/Message.java
@@ -1,36 +1,93 @@
 package domain.message;
 
+import domain.Member;
+
 import java.time.Instant;
+import java.util.Objects;
 
 public class Message {
 
-    //Определить способ присвоения уникального ID для каждого инстанса Message
-    private static int currentMessageID;
 
-    private final int messageID;
-    private final int creatorID;
-    private final Instant date;
-    private final MessageTypeInterface messageType;
+    private int messageID;
+//    private int messageCreatorID;
+    private Member messageCreatorMember;
+    private Instant date;
+    private String messageText;
+    private String messageType;
 
-    public Message(int creatorID, Instant date, MessageTypeInterface messageType) {
 
-        this.messageID = currentMessageID;
-        currentMessageID += 1;
-        this.creatorID = creatorID;
-        this.date = date;
+    public Message(Member messageCreatorMember, String messageText, String messageType) {
+
+        this.messageCreatorMember = messageCreatorMember;
+        this.messageText = messageText;
         this.messageType = messageType;
+    }
+    public Message() {
+
     }
 
     public int getMessageID() {
         return this.messageID;
     }
 
-    public int getCreatorID() {
-        return this.creatorID;
+//    public int getMessageCreatorID() {
+//        return this.messageCreatorID;
+//    }
+//
+//    public void setMessageCreatorID(int messageCreatorID) {
+//        this.messageCreatorID = messageCreatorID;
+//    }
+
+    public Member getMessageCreatorMember() {
+        return this.messageCreatorMember;
+    }
+    public void setMessageCreatorMember(Member messageCreatorMember) {
+        this.messageCreatorMember = messageCreatorMember;
     }
 
     public Instant getDate() {
         return this.date;
     }
 
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getMessageText() {
+        return this.messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageType() {
+        return this.messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return messageID == message.messageID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messageID);
+    }
+
+    @Override
+    public String toString() {
+        return "Author: " + this.messageCreatorMember.getUsername() + " " +
+               "Date: " + this.date.toString() + " " +
+               "Type: " + this.messageType + "\n" +
+               "Message: " + this.messageText;
+    }
 }
+
