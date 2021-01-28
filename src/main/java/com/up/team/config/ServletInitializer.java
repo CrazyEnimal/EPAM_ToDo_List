753da9921eb5ce20b32358ea45c3c26da456cce8
Админ <lambenteng@gmail.com>
2021-01-28 17:28:48 +0300

    Some fixes and reunion

--- src/main/java/com/up/team/config/ServletInitializer.java
+++ src/main/java/com/up/team/config/ServletInitializer.java
@@ -1,13 +1,14 @@
-package server;
+package com.up.team.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.access.SecurityConfig;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
+        return application.sources(SecurityConfig.class);
     }
 
 }
