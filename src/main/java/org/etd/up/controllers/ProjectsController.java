c8a05c671f1e273a967506d51c69481ad16082d8
isvink <isvink@gmail.com>
2021-02-18 17:36:30 +0300

    ProjectsController & mustache with Pavel supervision

--- src/main/java/org/etd/up/controllers/ProjectsController.java
+++ src/main/java/org/etd/up/controllers/ProjectsController.java
@@ -1,11 +1,33 @@
 package org.etd.up.controllers;
 
+import org.etd.up.models.Project;
+import org.etd.up.models.Task;
+import org.etd.up.repo.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectsController {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @GetMapping("/{id}")
+    public String getProject(@PathVariable int id, Map<String, Object> model) {
+        Project project = projectRepository.findById(id);
+        List<Task> tasks = project.getTasks();
+        model.put("project", project);
+        model.put("tasks", tasks);
+
+        return "project";
+
+    }
 
 
 }
